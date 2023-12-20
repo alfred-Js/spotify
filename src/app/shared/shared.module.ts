a8e83e724cd488b744cfaff2ea9584e98cc7eb76
AlfreJS <cjvirbox@gmail.com>
2023-12-20 12:39:58 -0300

    test msg

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { CardPlayerComponent } from './components/card-player/card-player.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, SidebarComponent, MediaPlayerComponent, HeaderUserComponent
+    CommonModule, SidebarComponent, MediaPlayerComponent, HeaderUserComponent, CardPlayerComponent
   ],
-  exports: [SidebarComponent, MediaPlayerComponent, HeaderUserComponent]
+  exports: [SidebarComponent, MediaPlayerComponent, HeaderUserComponent, CardPlayerComponent]
 })
 export class SharedModule { }
